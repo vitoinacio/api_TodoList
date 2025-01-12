56f852a578963292f31f8fc669026a5a5591b27d
Victor Hugo <victor.hugo.ina10@gmail.com>
2025-01-11 21:50:17 -0300

    banco hospedado e teste de conexao

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ import mysql from 'mysql';
 
 // cria a conexão com o banco de dados e exporta essa conexão;
 export const db = mysql.createConnection({
-  host: 'localhost',
+  host: 'monorail.proxy.rlwy.net',
   user: 'root',
-  password: 'root',
-  database: 'todolist',
+  password: 'kBurJkyPjyFfosFvSebSzCLFISeqPqbt',
+  database: 'railway',
 });
