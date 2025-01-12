2ecf33c1e2bb05ea1d48725c1621d36987fe321c
Victor Hugo <victor.hugo.ina10@gmail.com>
2025-01-12 08:44:17 -0300

    feat: atualizacao do index.js

--- db.js
+++ db.js
@@ -5,12 +5,30 @@ dotenv.config();
 
 const dbUrl = process.env.DATABASE_URL;
 
-export const db = mysql.createConnection(dbUrl);
+let db;
 
-db.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar a database:', err);
-    return;
-  }
-  console.log('Conectado a database.');
-});
+function handleDisconnect() {
+  db = mysql.createConnection(dbUrl);
+
+  db.connect((err) => {
+    if (err) {
+      console.error('Erro ao conectar a database:', err);
+      setTimeout(handleDisconnect, 2000); // Reconnect after 2 seconds
+    } else {
+      console.log('Conectado a database.');
+    }
+  });
+
+  db.on('error', (err) => {
+    console.error('Erro na database:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.fatal) {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
+export { db };
